a58f39467bcdd0af90b57455c837209281ba79ab
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2024-12-17 18:01:17 +0530

    sdcard-images-utils: nxp: runme.sh: set default labe name as MOD-ADSD3500-SPI
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- sdcard-images-utils/nxp/runme.sh
+++ sdcard-images-utils/nxp/runme.sh
@@ -156,7 +156,7 @@ echo "u-boot		$UBOOT_NXP_REL" >> $ROOTDIR/images/sw-versions
 
 echo "MENU TITLE ADITOF boot options" > $ROOTDIR/images/extlinux.conf
 echo "TIMEOUT 10" >> $ROOTDIR/images/extlinux.conf
-echo "DEFAULT ADSD3500+ADSD3100" >> $ROOTDIR/images/extlinux.conf
+echo "DEFAULT MOD-ADSD3500-SPI" >> $ROOTDIR/images/extlinux.conf
 echo "label ADSD3100" >> $ROOTDIR/images/extlinux.conf
 echo "        linux ../Image" >> $ROOTDIR/images/extlinux.conf
 echo "        fdt ../imx8mp-adi-tof-noreg.dtb" >> $ROOTDIR/images/extlinux.conf
@@ -198,6 +198,14 @@ else
 	echo "        append root=/dev/mmcblk1p2 rootwait ro init=/usr/lib/init_resize.sh" >> $ROOTDIR/images/extlinux.conf
 fi
 
+echo "label MOD-ADSD3500-SPI" >> $ROOTDIR/images/extlinux.conf
+echo "        linux ../Image" >> $ROOTDIR/images/extlinux.conf
+echo "        fdt ../imx8mp-adi-tof-module-adsd3500-spi.dtb" >> $ROOTDIR/images/extlinux.conf
+if [[ $BUILD_TYPE == "buildroot" ]]; then
+	echo "        initrd ../rootfs.cpio.uboot" >> $ROOTDIR/images/extlinux.conf
+else
+	echo "        append root=/dev/mmcblk1p2 rootwait ro init=/usr/lib/init_resize.sh" >> $ROOTDIR/images/extlinux.conf
+fi
 
 mmd -i tmp/part1.fat32 ::/extlinux
 mcopy -i tmp/part1.fat32 $ROOTDIR/images/sw-versions ::/sw-versions
